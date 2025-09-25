d1d0d70aa5959a068dab26d0533d045a47bf70a3
pdcherry <pcherry@protonmail.ch>
2025-09-24 22:18:16 -0700

    add some practice for SQL

--- R/1_load_first_duckdb.R
+++ R/1_load_first_duckdb.R
@@ -25,4 +25,30 @@ con <- dbConnect(duckdb(),
 test_query <- "SELECT * FROM airlines LIMIT 10"
 
 # Run the following on your own:
-# dbGetQuery(con, test_query)
+dbGetQuery(con, test_query)
+dbGetQuery(con,
+           "SELECT * FROM pg_tables")
+dbGetQuery(con, "FROM flights LIMIT 10")
+dbGetQuery(con,
+           "SELECT carrier, MEAN(dep_delay) AS mean_dep_delay, COUNT(*) AS N_flights FROM flights
+           GROUP BY carrier
+           WHERE mean_dep_delay < 50
+           ORDER BY N_flights DESC")
+dbGetQuery(con,
+           "SELECT year, month, COUNT(*) AS N_flights FROM flights
+           GROUP BY year, month")
+dbGetQuery(con,
+           "SELECT distance, year, month, day,
+           SUM(distance) AS cumu_distance
+           OVER ( ORDER BY year, month, day )
+           FROM flights")
+
+# Find the time stamp (ts) of the 7th something above 4 hours for each airline
+dbGetQuery(con,
+           "SELECT carrier, concat_ws(' ', year, month, day, dep_time) AS ts FROM (
+              SELECT *, row_number() OVER (PARTITION BY carrier ORDER BY year, month, day, dep_time) AS nth_flight FROM flights
+              WHERE air_time >= 240
+           )
+           WHERE nth_flight == 7")
+
+
